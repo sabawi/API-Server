ccc479459ae1b7bedd48b87b91bad66a2fc1a66f
sabawi <Bad4cics>
2018-02-10 17:35:45 -0500

    Initial Commit

---
+++ API-Server/app/api/authenticate.py
@@ -0,0 +1,14 @@
+from flask import request
+
+from app import app
+
+
+@app.route('/authenticate', methods=['GET', 'POST'])
+def authenticate():
+    content = request.get_json()
+    authentication = 'Failed'
+    if content['userid'] == 'myusername' and content['password'] == 'password123':
+        authentication = 'Success'
+
+    print(authentication)
+    return '\n' + authentication + '\n'
