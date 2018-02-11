fd9c6e076d23219478fd1a1ffb403caebb7f41c5
sabawi <Bad4cics>
2018-02-11 11:01:13 -0500

    Add API for authentication

--- API-Server/app/api/authenticate.py
+++ API-Server/app/api/authenticate.py
@@ -4,6 +4,8 @@ from app import app
 
 @app.route('/authenticate', methods=['POST'])
 def authenticate():
+    # Test this API with curl:
+    # curl -d '{"userid":"myusername", "password":"password123"}' -H "Content-Type: application/json" -X  POST http://localhost:8080/authenticate
     content = request.get_json()
     authentication = 'Failed'
     if content['userid'] == 'myusername' and content['password'] == 'password123':
--- API-Server/py-APIServer.py
+++ API-Server/py-APIServer.py
@@ -1,4 +1,7 @@
 from app import app
 
 if __name__ == "__main__":
+    import socket
+
+    print(socket.gethostbyname_ex(socket.gethostname())[0])
     app.run(port=8080)
